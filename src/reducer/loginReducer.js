671adf2d0b23bc5a56134e790fe595172dab2868
vedo18 <saketvats18@gmail.com>
2024-06-11 23:54:19 +0530

    Added login state files in redux saga

---
+++ src/reducer/loginReducer.js
@@ -0,0 +1,43 @@
+import {
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from '../constants/loginConstants';
+
+const initialState = {
+  isLoading: false,
+  isAuthenticated: false,
+  user: null,
+  error: null,
+};
+
+const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        error: null,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isAuthenticated: true,
+        user: action.payload,
+        error: null,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default loginReducer;
