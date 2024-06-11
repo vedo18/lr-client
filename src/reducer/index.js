e73c18e7fb2d6384ce5272d6f4c6cd8590d3a3ed
vedo18 <saketvats18@gmail.com>
2024-06-11 23:56:29 +0530

    updated login to auth for folder names and updated reducer for loginn

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,5 +1,10 @@
 import {combineReducers} from 'redux';
 
-const createRootReducer = () => combineReducers({});
+import loginReducer from './loginReducer';
+
+const createRootReducer = () =>
+  combineReducers({
+    auth: loginReducer,
+  });
 
 export default createRootReducer;
